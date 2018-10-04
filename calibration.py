3e97922d89c17607010619cd918baddc240c1d47
Daniel P <43250695+callmed@users.noreply.github.com>
2018-10-04 10:45:29 +0200

    Added functions to convert physical values
    Convert a float value into a IEEE-754 valid value.

--- calibration.py
+++ calibration.py
@@ -10,3 +10,22 @@ if CAN_INTERFACE not NULL:
     can.util.load_environment_config()
 
 bus = can.interface.Bus()
+
+
+def convertIEEE754Hex(value):
+"""Converts a float value into a IEEE754 hex representative value."""
+    hex_value = None
+    int_value = None
+    int_value = np.float32(float(value)).view(np.int32).item()
+    hex_value = hex(int_value)
+    return hex_value
+
+
+def loadDataToMessage(payload):
+"""Re-orders data to fit the INTEL byte-order rule (little-endian)."""
+    # Least significant byte at least significat message byte
+    return payload
+
+
+def swapPayload(payload):
+    pass

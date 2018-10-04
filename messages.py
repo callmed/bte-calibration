cf110000b014877be82a86b8d87fb10a872f1c72
Daniel P <43250695+callmed@users.noreply.github.com>
2018-10-04 21:30:27 +0200

    Message generating func added
    Added a function to load data (payload) into a message and return the ready-to-use message object.

--- messages.py
+++ messages.py
@@ -5,13 +5,54 @@ Message basics: The data on CAN bus are sorted by INTEL byte-order, means least-
 message byte order.
 E.g. \x00\x00\x20\x03 represents 0x0320
 
+References:
 The data is often converted to IEEE float what is a special kind of coding integer values.
 Convertion described: https://stackoverflow.com/questions/24289180/how-to-convert-a-hex-string-into-an-unpacked-ieee-754-format-number
 Converter: https://www.h-schmidt.net/FloatConverter/IEEE754de.html
+STRUCT:
+https://docs.python.org/3.6/library/struct.html
+https://pymotw.com/2/struct/
+
 """
 
+
+import binascii
+import struct
 from can import Message
 
+
+SET_SLOPE_U_I = {"msg_id": 0x144,
+                 "msg_fmt": "<ff",
+                 "dlc": 8,
+                 "channels": [{"name": "slope_voltage",
+                               "unit": "V",
+                               "factor: 1},
+                               {"name": "slope_current",
+                                "unit": "A",
+                                "factor": 1
+                               }]
+                }
+                              
+
+MSGID_SET_SLOPE_U_I = 0x144  
+
+
+def msgSET_SLOPE_U_I(slope_voltage,slope_current):
+  """Create CAN message object with data loaded.""" 
+  payload = None
+  fmt = '<ff'
+  payload = struct.pack(fmt, slope_voltage, slope_current)
+  return Message(arbitration_id=;MSGID_SET_SLOPE_U_I,data=payload,extended_id=False)
+
+# To unload data from message                             
+# voltage,current = struct.unpack(fmt,payload)
+                              
+
+                              
+                              #
+                              #   O B S O L E T E
+                              #
+                              
 #TODO: Wrong data load in messages. Read description text about byte order and data conversion
 msgSET_SLOPE_UI = Message(arbitration_id=324, extended_id=False, data=bytearray(b'\x00\x00\x00\xC8\x00\x00\x00\xC8'))
 msgSET_PWR_SLOPE = Message(arbitration_id=340, extended_id=False, data=bytearray(b'\x00\x00\x00\x00\x00\x00\x00\xC8'))
